205f49d0dba3d02a0893b501a0a2f054c47b5dd3
MatheusCod <matheus.log100@gmail.com>
2022-02-13 15:54:30 -0300

    refactor: add SignUpUseCase

--- login.js
+++ login.js
@@ -1,19 +1,29 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
-const AccountModel = mongoose.model('Account')
 
 module.exports = () => {
   router.post('/signup', new SignUpRouter().route)
 }
 
+// signup-router
+const express = require('express')
+const router = express.Router()
+
 class SignUpRouter {
   async route (req, res) {
     const { email, password, repeatPassword } = req.body
+    new SignUpUseCase().signUp(email, password, repeatPassword)
+    res.status(400).json({ error: 'password must be equal to repeatPassword' })
+  }
+}
+
+// signup-usecase
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+
+class SignUpUseCase {
+  async signUp (email, password, repeatPassword) {
     if (password === repeatPassword) {
       const user = await AccountModel.create({ email, password })
-      return res.json(user)
+      return user
     }
-    res.status(400).json({ error: 'password must be equal to repeatPassword' })
   }
 }
