a1bcab28a7a6a1ff77f175ecaa38b1924b4d9203
MatheusCod <matheus.log100@gmail.com>
2022-02-13 13:55:23 -0300

    refactor: create SignUpRouter

--- login.js
+++ login.js
@@ -4,12 +4,16 @@ const mongoose = require('mongoose')
 const AccountModel = mongoose.model('Account')
 
 module.exports = () => {
-  router.post('/signup', async (req, res) => {
+  router.post('/signup', new SignUpRouter().route)
+}
+
+class SignUpRouter {
+  async route (req, res) {
     const { email, password, repeatPassword } = req.body
     if (password === repeatPassword) {
       const user = await AccountModel.create({ email, password })
       return res.json(user)
     }
     res.status(400).json({ error: 'password must be equal to repeatPassword' })
-  })
+  }
 }
